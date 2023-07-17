47fa2e68a17da50417e5c19a459917cf0c927e7a
arturgontijo <arturgontijo@users.noreply.github.com>
2023-07-17 13:09:14 +0000

    deploy: 211eead5cc3772afac9ab5e55df6df509ce9fcd6

--- bridge_hub_test_utils/sidebar-items.js
+++ bridge_hub_test_utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["test_cases","Module contains predefined test-case scenarios for `Runtime` with bridging capabilities."]],"struct":[["CollatorSessionKeys",""]]};
+window.SIDEBAR_ITEMS = {"enum":["XcmReceivedFrom"],"fn":["assert_metadata","assert_total","mock_open_hrmp_channel","test_header"],"mod":["test_cases"],"struct":["CollatorSessionKeys","ExtBuilder","RuntimeHelper"],"type":["AccountIdOf","BalanceOf","SessionKeysOf","ValidatorIdOf"]};
